00ccbc55585d16f9f66554b4a1f324d254cdc211
Abishek1613 <79819479+Abishek1613@users.noreply.github.com>
2022-03-10 16:46:53 +0530

    Add files via upload

---
+++ Lesson1/src/main/java/Phase5/Lesson1/App.java
@@ -0,0 +1,13 @@
+package Phase5.Lesson1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
